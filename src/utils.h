a1dfe19eccee6b95bfd7990d26bb3ee6bf4e96fe
Elvin Holst <elvho551@student.liu.se>
2026-07-25 23:57:00 +0200

    Implement TYPEOF macro

--- src/utils.h
+++ src/utils.h
@@ -10,7 +10,7 @@
 
 #define allocate(a, t, count) (t *)((a).function((a).context, 0, 0, (count), sizeof(t), ALIGNOF(t)))
 #define reallocate(a, ptr, old_count, new_count) (a).function((a).context, (ptr), (old_count), \
-        (new_count), sizeof(*(ptr)), ALIGNOF(*(ptr)))
+        (new_count), sizeof(*(ptr)), ALIGNOF(TYPEOF(*(ptr))))
 #define deallocate(a, ptr, count) (a).function((a).context, (ptr), (count), 0, sizeof(*(ptr)), ALIGNOF(*(ptr)))
 
 #define str_from_span(span) (memmi_String) {(span).data, (span).count}
@@ -19,6 +19,13 @@
 
 // TODO: use __builtin_trap or similar instead
 
+#if defined(__GNUC__)
+#    error
+#elif defined(_MSC_VER)
+#    define TYPEOF(t) __typeof__(t)
+#else
+#    error TYPEOF undefined for compiler
+#endif
 
 #if defined(__GNUC__)
 #    error
