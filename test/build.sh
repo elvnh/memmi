c5089632577ab6e84296f9337d1921610d4b57ea
elvnh <135953306+elvnh@users.noreply.github.com.>
2026-09-18 10:25:02 +0200

    Allow running all test cases from build script

--- test/build.sh
+++ test/build.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 CC="gcc"
 CFLAGS="-Wall -Wextra -ggdb -I../include/ -Isrc/framework"
 
 BUILD_DIR="build"
+CASES_DIR="${BUILD_DIR}/cases"
 DEBUGGEE_EXE="debuggee"
 DEBUGGEE_PATH="${BUILD_DIR}/${DEBUGGEE_EXE}"
 TEST_RUNNER_PATH="${BUILD_DIR}/test_runner"
@@ -11,7 +12,7 @@ TEST_RUNNER_PATH="${BUILD_DIR}/test_runner"
 cd $(dirname $0);
 
 rm -r ${BUILD_DIR} 2> /dev/null;
-mkdir -p "${BUILD_DIR}/cases";
+mkdir -p ${CASES_DIR};
 
 ${CC} ${CFLAGS} src/debuggee_main.c -o ${DEBUGGEE_PATH};
 ${CC} ${CFLAGS} src/test_runner.c  -o ${TEST_RUNNER_PATH};
@@ -22,6 +23,10 @@ for file in src/cases/*.c; do
     name=$(basename ${file})
     name_without_extension=${name%.*}
 
-    ${CC} ${CFLAGS} ${file} -o "build/cases/"${name_without_extension} \
+    ${CC} ${CFLAGS} ${file} -o "${CASES_DIR}/${name_without_extension}" \
           -DDEBUGGEE_EXECUTABLE_NAME="\"${DEBUGGEE_EXE}\""
 done
+
+if [[ "$1" == "run" ]]; then
+    ./${TEST_RUNNER_PATH} ${CASES_DIR}/*
+fi
