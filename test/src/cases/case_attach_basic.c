72e6892d1fe0d39c2be6296718ad32b867ce669f
elvnh <135953306+elvnh@users.noreply.github.com>
2026-09-20 02:21:50 +0200

    Test case for attaching and detaching

---
+++ test/src/cases/case_attach_basic.c
@@ -0,0 +1,11 @@
+#include "test_case_base.c"
+
+void test_case_main(Pid pid, Ipc ipc)
+{
+    memmi_Process proc = memmi_open_process(pid).process;
+    memmi_Status attach_result = memmi_attach_to_process(proc);
+    REQUIRE(attach_result == MEMMI_OK);
+
+    memmi_Status detach_result = memmi_detach_from_process(proc);
+    REQUIRE(detach_result == MEMMI_OK);
+}
